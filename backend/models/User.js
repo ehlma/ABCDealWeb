cc3a745b04a287a40c1df73638516c191fe47dc3
elineviktoria <eline@tobiassen.no>
2025-05-24 23:41:33 +0200

    koblet sammen controller og routes med server

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,13 @@
 import mongoose, { mongo } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+
 
 const userSchema = new mongoose.Schema({
     userId: {
         type: String, 
         required: true, 
         unique: true,
-        default: uuid4, // genererer unik id automatisk (i backend: npm install uuid)    
+        default: uuidv4, // genererer unik id automatisk (i backend: npm install uuid)    
     },
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
