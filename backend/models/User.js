27bd928b6bb2f430c05e9264a16fa795b4e668a2
elineviktoria <eline@tobiassen.no>
2025-05-24 22:47:02 +0200

    added users.js model

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: String, 
+        required: true, 
+        unique: true,
+        default: uuid4, // genererer unik id automatisk (i backend: npm install uuid)    
+    },
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    passwordHash: {type: String, required: true},
+    role: {type: String, required: true},
+    createdAt: {type: String, default: Date.now},
+});
+
+export default mongoose.model('User', userSchema);
