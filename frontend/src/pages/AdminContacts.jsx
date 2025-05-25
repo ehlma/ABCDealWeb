86005ae86f5d876c1a1c304bb218fd65ca3007c1
elineviktoria <eline@tobiassen.no>
2025-05-25 21:25:13 +0200

    added admincontact.js frontend

---
+++ frontend/src/pages/AdminContacts.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+const AdminContacts = () => {
+    const [contacts, setContacts] = useState([]);
+    const [error, setError] = useState([]);
+
+    // sett inn JWT (token)
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        const fetchContacts = async () => {
+            try {
+                const res = await axios.get("http://localhost:5050/api/contact", { // NB: endre url til variabel?
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                setContacts(res, data);
+            } catch (error) {
+                setError("Could not fetch contact forms");
+            }
+        };
+
+        fetchContacts();
+    }, [])
+
+    return (
+        <div>
+            <h2>Innsendte meldinger</h2>
+            {error && <p>{error}</p>}
+            <ul>
+                {contacts.map((msg) => (
+                    <li key={msg._id}>
+                        <strong>{msg.name}</strong> ({msg.email}, {msg.phoneNum})<br/>
+                        {msg.text}<br/>
+                        {msg.image && <img src={msg.image} alt="Vedlegg" width="100"/>}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default AdminContacts;
