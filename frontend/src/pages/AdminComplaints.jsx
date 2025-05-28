71ca193e67801929c71ab168540d53f61a235d09
elineviktoria <eline@tobiassen.no>
2025-05-28 17:18:57 +0200

    test

--- frontend/src/pages/AdminComplaints.jsx
+++ frontend/src/pages/AdminComplaints.jsx
@@ -8,6 +8,22 @@ const AdminComplaints = () => {
     const [complaints, setComplaints] = useState([]);
     const [error, setError] = useState("");
 
+    // 1. funksjon for å hente oppdaterte reklamasjoner
+    const fetchComplaints = async () => {
+        try {
+            const res = await api.get(API_ENDPOINTS.complaints);
+            setComplaints(res.data);
+        } catch (err) {
+            setError("Kunne ikke hente reklamasjon. ")
+        }
+    };
+
+    // 2. hent reklamasjoner ved første lasting
+    useEffect(() => {
+        fetchComplaints();
+    }, []);
+
+    // 3. sortering av reklamasjoner etter status-prioritet
     const getPriority = (status) => {
         if (status === "new") return 0;
         if (status === "pending") return 1;
@@ -17,18 +33,6 @@ const AdminComplaints = () => {
     const sortedComplaints = [...complaints].sort(
         (a, b) => getPriority(a.status) - getPriority(b.status)
     );
-
-    useEffect(() => {
-        const fetchComplaints = async () => {
-            try {
-                const res = await api.get(API_ENDPOINTS.complaints);
-                setComplaints(res.data);
-            } catch (err) {
-                setError("Kunne ikke hente reklamasjon. ")
-            }
-        };
-        fetchComplaints();
-    }, []);
 
     return (
         <div className="max-w-2x1 mx-auto mt-6">
@@ -47,8 +51,9 @@ const AdminComplaints = () => {
                         image: c.image,
                         hasAttachment: !!c.image,
                         docs: c.documentation || [],
-                        status: c.status || "new",
+                        status: c.status || "new"
                     }}
+                    onStatusChange={fetchComplaints} // 4. send prop
                 />
             ))}
         </div>
