f2c8a9e2a97721a7c05b62c663592350d5755b98
helletomten <152817250+helletomten@users.noreply.github.com>
2025-05-29 17:20:10 +0200

    header logo

--- frontend/src/pages/AdminLayout.jsx
+++ frontend/src/pages/AdminLayout.jsx
@@ -1,9 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
+import logo from "../assets/Logo.png";
 
 const AdminLayout = () => {
     return (
         <div className="min-h-screen flex flex-col">
-            <nav className="fixed top-0 left-0 bg-[#047464] w-full text-white px-6 py-4 shadow">
+            <nav className="fixed top-0 z-[50] left-0 bg-[#047464] w-full text-white px-6 py-4 shadow flex items-center">
                 <ul className="flex gap-6 font-medium">
                     <li>
                         <Link to="/admin/settings" className="hover:underline hover:text-[#a7f3d0] transition-colors text-white">
@@ -20,13 +21,14 @@ const AdminLayout = () => {
                             Reklamasjon
                         </Link>
                     </li>
-
                 </ul>
-            <div className="pointer-events-none fixed top-[55px] left-0 w-full h-20 z-10 bg-gradient-to-b from-white/90 to-transparent"></div>
+                <img src={logo} alt="ABC Deal Logo" className="h-[36px] ml-auto" />
             </nav>
 
+            <div className="pointer-events-none fixed top-[68px] left-0 w-full h-20 z-10 bg-gradient-to-b from-white/90 to-transparent"></div>
 
-            <main className="flex-1 p-6 overflow-y-auto">
+
+            <main className="flex-1 pt-[96px] px-[24px] pb-[24px] overflow-y-auto">
                 <Outlet/>
             </main>
         </div>
--- frontend/src/pages/AdminSettings.jsx
+++ frontend/src/pages/AdminSettings.jsx
@@ -84,19 +84,19 @@ const AdminSettings = () => {
     };
 
     return (
-        <div className="w-full grid grid-cols-3 gap-4 px-4 max-w-screen-xl mx-auto">
+        <div className="w-full grid grid-cols-3 gap-[16px] px-[8px] max-w-screen-xl mx-auto">
           {/* Tom kolonne venstre */}
           <div></div>
       
           {/* Skjema  */}
-          <div className="mt-12 col-span-3 sm:col-span-3 md:col-span-1 px-4">
-            {error && <p className="text-red-600 font-medium mb-4">{error}</p>}
-            <h3 className="text-xl mb-4">Legg til ny ansatt</h3>
+          <div className="mt-[48px] col-span-3 sm:col-span-4 md:col-span-1 px-[16px]">
+            {error && <p className="text-red-600 font-medium mb-[16px]">{error}</p>}
+            <h3 className="text-xl mb-[16px]">Legg til ny ansatt</h3>
             <form onSubmit={handleSubmit}>
               <input
                 type="text"
                 placeholder="Fornavn"
-                className="border border-gray-300 rounded px-3 py-2 mb-2 w-full"
+                className="border border-gray-300 rounded px-[12px] py-[8px] mb-[8px] w-full"
                 value={formData.firstName}
                 onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
                 required
@@ -104,7 +104,7 @@ const AdminSettings = () => {
               <input
                 type="text"
                 placeholder="Etternavn"
-                className="border border-gray-300 rounded px-3 py-2 mb-2 w-full"
+                className="border border-gray-300 rounded px-[12px] py-[8px] mb-[8px] w-full"
                 value={formData.lastName}
                 onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
                 required
@@ -112,7 +112,7 @@ const AdminSettings = () => {
               <input
                 type="email"
                 placeholder="E-post"
-                className="border border-gray-300 rounded px-3 py-2 mb-2 w-full"
+                className="border border-gray-300 rounded px-[12px] py-[8px] mb-[8px] w-full"
                 value={formData.email}
                 onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                 required
@@ -122,17 +122,17 @@ const AdminSettings = () => {
                   type={showPassword ? "text" : "password"}
                   placeholder="Passord"
                   value={formData.password}
-                  className="w-full border border-gray-300 rounded px-3 py-2 pr-10"
+                  className="w-full border border-gray-300 rounded px-[12px] py-[8px] pr-[40px]"
                   onChange={(e) => setFormData({ ...formData, password: e.target.value })}
                   required
                 />
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="absolute top-1/2 right-2 -translate-y-1/2 p-0 bg-transparent border-none cursor-pointer focus:outline-none"
+                  className="absolute top-1/2 right-[8px] -translate-y-1/2 p-0 bg-transparent border-none cursor-pointer focus:outline-none"
                   aria-label={showPassword ? "Skjul passord" : "Vis passord"}
                 >
-                  {showPassword ? <EyeOff className="w-5 h-5 text-gray-500" /> : <Eye className="w-5 h-5 text-gray-500" />}
+                  {showPassword ? <EyeOff className="w-[20px] h-[20px] text-gray-500" /> : <Eye className="w-5 h-5 text-gray-500" />}
                 </button>
               </div>
       
@@ -142,7 +142,7 @@ const AdminSettings = () => {
                   value={formData.role}
                   onChange={(e) => setFormData({ ...formData, role: e.target.value })}
                   required
-                  className="w-full border border-gray-300 rounded px-3 py-2 mb-2 appearance-none"
+                  className="w-full border border-gray-300 rounded px-[12px] py-[8px] mb-[8px] appearance-none"
                 >
                   {roles.map((role) => (
                     <option key={role.value} value={role.value}>
@@ -150,12 +150,12 @@ const AdminSettings = () => {
                     </option>
                   ))}
                 </select>
-                <ChevronDown className="absolute right-2 top-[45%] -translate-y-1/2 text-gray-500 pointer-events-none h-5 w-5" />
+                <ChevronDown className="absolute right-[8px] top-[45%] -translate-y-1/2 text-gray-500 pointer-events-none h-[20px] w-[20px]" />
               </div>
       
               <button
                 type="submit"
-                className="mt-4 bg-blue-100 font-medium py-2 px-4 rounded hover:bg-blue-200"
+                className="mt-[8px] bg-blue-100 font-medium py-[8px] px-[16px] rounded hover:bg-blue-200"
               >
                 Opprett ansatt
               </button>
@@ -166,43 +166,43 @@ const AdminSettings = () => {
           <div></div>
       
           {/* Ansattkort - tar hele bredden under grid */}
-          <div className="col-span-3 mt-12 w-full">
-            <h2 className="text-xl mb-4 inline-block pb-1 shadow-[0_4px_2px_-2px_rgba(0,0,0,0.2)]">Ansatte</h2>
-            <ul className="grid grid-cols-1 gap-6">
+          <div className="col-span-3 mt-[48px] w-full">
+            <h2 className="text-xl mb-[16px] inline-block pb-[4px] shadow-[0_4px_2px_-2px_rgba(0,0,0,0.2)]">Ansatte</h2>
+            <ul className="grid grid-cols-1 gap-[24px]">
               {users.map((user) => (
-                <li key={user._id} className="bg-white shadow rounded p-6 w-full">
+                <li key={user._id} className="bg-white shadow rounded p-[24px] w-full">
                   {editUserId === user._id ? (
-                    <div className="space-y-2">
-                      <input name="firstName" value={editData.firstName} onChange={handleEditChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
-                      <input name="lastName" value={editData.lastName} onChange={handleEditChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
-                      <input name="email" value={editData.email} onChange={handleEditChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
-                      <select name="role" value={editData.role} onChange={handleEditChange} className="border border-gray-300 rounded px-3 py-2 w-full">
+                    <div className="space-y-[8px]">
+                      <input name="firstName" value={editData.firstName} onChange={handleEditChange} className="border border-gray-300 rounded px-[12px] py-[8px] w-full" />
+                      <input name="lastName" value={editData.lastName} onChange={handleEditChange} className="border border-gray-300 rounded px-[12px] py-[8px] w-full" />
+                      <input name="email" value={editData.email} onChange={handleEditChange} className="border border-gray-300 rounded px-[12px] py-[8px] w-full" />
+                      <select name="role" value={editData.role} onChange={handleEditChange} className="border border-gray-300 rounded px-[12px] py-[8px] w-full">
                         {roles.map((role) => (
                           <option key={role.value} value={role.value}>
                             {role.label}
                           </option>
                         ))}
                       </select>
-                      <div className="flex flex-row justify-center gap-2">
-                        <button onClick={() => handleUpdate(user._id)} className="bg-blue-500 text-white px-2 py-1 text-sm rounded hover:bg-blue-600 mr-2">Lagre</button>
-                        <button onClick={() => setEditUserId(null)} className="bg-gray-300 text-gray-800 px-2 py-1 text-sm rounded hover:bg-gray-400">Avbryt</button>
+                      <div className="flex flex-row justify-center gap-[8px]">
+                        <button onClick={() => handleUpdate(user._id)} className="bg-blue-500 text-white px-[8px] py-[4px] text-sm rounded hover:bg-blue-600 mr-[8px]">Lagre</button>
+                        <button onClick={() => setEditUserId(null)} className="bg-gray-300 text-gray-800 px-[8px] py-[4px] text-sm rounded hover:bg-gray-400">Avbryt</button>
                       </div>
                     </div>
                   ) : (
-                    <div className="flex flex-col items-center sm:flex-row sm:justify-between sm:items-center gap-4">
-                      <p className="mb-2">{user.firstName} {user.lastName} - {user.email} ({user.role})</p>
+                    <div className="flex flex-col items-center sm:flex-row sm:justify-between sm:items-center gap-[16px]">
+                      <p className="mb-[8px]">{user.firstName} {user.lastName} - {user.email} ({user.role})</p>
 
                       <div className="flex flex-row justify-center">
                         {deleteConfirmId === user._id ? (
                           <>
                             <p className="text-blue-500">Er du sikker?</p>
-                            <button onClick={() => handleDelete(user._id)} className="bg-green-500 text-white text-sm px-2 py-1 rounded hover:bg-green-600 mr-2">Aksepter</button>
-                            <button onClick={() => setDeleteConfirmId(null)} className="ml-2 bg-red-500 text-white text-sm px-2 py-1 rounded hover:bg-red-600">Avbryt</button>
+                            <button onClick={() => handleDelete(user._id)} className="bg-green-500 text-white text-sm px-[8px] py-[4px] rounded hover:bg-green-600 mr-[8px]">Aksepter</button>
+                            <button onClick={() => setDeleteConfirmId(null)} className="ml-[8px] bg-red-500 text-white text-sm px-[8px] py-[4px] rounded hover:bg-red-600">Avbryt</button>
                           </>
                         ) : (
                           <>
-                            <button onClick={() => setDeleteConfirmId(user._id)} className="ml-2 px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600 text-sm">Slett</button>
-                            <button onClick={() => handleEdit(user)} className="ml-2 bg-gray-200 text-gray-800 px-2 py-1 rounded hover:bg-gray-300 text-sm">Rediger</button>
+                            <button onClick={() => setDeleteConfirmId(user._id)} className="ml-[8px] px-[8px] py-[4px] bg-red-500 text-white rounded hover:bg-red-600 text-sm">Slett</button>
+                            <button onClick={() => handleEdit(user)} className="ml-[4px] bg-gray-200 text-gray-800 px-[8px] py-[4px] rounded hover:bg-gray-300 text-sm">Rediger</button>
                           </>
                         )}
                       </div>
