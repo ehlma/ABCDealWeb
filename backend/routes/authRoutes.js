15a96dc635b7aaf5823ced1c78a2da40be728814
helletomten <152817250+helletomten@users.noreply.github.com>
2025-06-04 00:12:44 +0200

    backend reset password
    bruker trykker glemt passord, skriver inn mailen sin i feltet, mottar mail med en lenke hvor de kan oppdatere passord

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { sendResetLink, setNewPassword } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/reset-password", sendResetLink);
+router.post("/set-new-password", setNewPassword);
+
 
 export default router;
