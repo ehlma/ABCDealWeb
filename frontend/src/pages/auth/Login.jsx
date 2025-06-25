2886a63ae86b3339f3997289eaecefeaee3835ec
helletomten <152817250+helletomten@users.noreply.github.com>
2025-06-25 13:38:27 +0200

    bugs med import fikset

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import {useNavigate} from "react-router-dom";
-import api, {API_ENDPOINTS} from "../../api";
+import api, {API_ENDPOINTS} from "../../../api/api.js";
 import '../../index.css';
 import logo from "../../assets/Logo.png";
 import { useAuth } from "../../contexts/AuthContext";
--- frontend/src/pages/auth/ResetPassword.jsx
+++ frontend/src/pages/auth/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../../api";
+import api from "../../../api/api.js";
 import {Eye, EyeOff} from "lucide-react";
 
 const ResetPassword = () => {
