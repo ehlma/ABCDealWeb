fcec50dabe9ed555437222fdf2de35ecbcd18f73
helletomten <152817250+helletomten@users.noreply.github.com>
2025-06-26 23:05:52 +0200

    artikler
    vise artikler på landingssiden, piler på articlepage

--- frontend/src/pages/client/HomePage.jsx
+++ frontend/src/pages/client/HomePage.jsx
@@ -19,24 +19,29 @@ const HomePage = () => {
     }, []);
 
     return (
-        <div className="p-8 text-center">
+        <div className="text-center">
             <h1 className="text-4xl font-bold mb-4 text-[047464]">Velkommen til ABC Deal</h1>
-            <p className="text-lg text-gray-700">Din pålitelige forhandler av campongvogner og bobiler.</p>
+            <p className="text-lg text-gray-700">Din pålitelige forhandler av biler, campongvogner og bobiler.</p>
             <div className="mt-8 p-8 bg-blue-100 rounged-lg shadow">
                 <p className="text-xl text-blue-800">Utforsk vårt utvalg av bobiler for ditt neste eventyr!</p>
             </div>
 
-            <h2>Aktuelt</h2>
-            <div>
+            <h2 className="text-2xl font-semibold mt-12 mb-3">Aktuelt</h2>
+            <div className="grid gap-6 md:grid-cols-2">
                 {articles.map((article) => (
-                    <div key={article._id}>
-                        <h3>{article.title}</h3>
+                    <div key={article._id} className="bg-white rounded shadow-md overflow-hidden py-4">
+                        <h3 className="text-lg mb-2">{article.title}</h3>
                         {article.images && article.images[0] && (
-                            <img src={article.images[0]} alt="" />
+                            <img 
+                                src={article.images[0]} 
+                                alt={article.title} 
+                                className="w-full h-48 object-cover"
+                            />
                         )}
-                        <p>{article.intro}</p>
+                        <p className="text-gray-700 text-sm mb-3 mt-4">{article.intro}</p>
                         <Link
                             to={`/articles/${article._id}`}
+                            className="text-blue-600 font-medium hover:underline"
                         >
                             Les mer →
                         </Link>
