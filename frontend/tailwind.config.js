14825ac86729864ceb4afcb80002f57158051e4b
elineviktoria <eline@tobiassen.no>
2025-07-11 12:47:27 +0200

    update to nav bar color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,12 @@ export default {
           "primary-dark": "#065f54",
           "ui-background": "#34495e",
           // "footer-background": "#2c3e50",
+
+          "navbar-bg": "#F8F8F8",
+          "navbar-link": "#333333",
+          "navbar-link-hover": "#047464",
+          "navbar-link-active": "#047464",
+          "navbar-icon": "#333333",
         },
         fontFamily: { // Egne fonter
           heading: ["var(--font-heading)", "sans-serif"], // Poppins for overskrifter
