d3f156a169c8a8aa054061958ca094b263e39974
helletomten <152817250+helletomten@users.noreply.github.com>
2025-08-04 12:37:51 +0200

    complaint styling

--- frontend/src/pages/client/ComplaintsPage.jsx
+++ frontend/src/pages/client/ComplaintsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api, { API_ENDPOINTS } from "../../../api/api.js";
+import complaintImage from "../../assets/complaintImgs/complaintImage.jpeg";
 
 const ComplaintsPage = () => {
     const [formData, setFormData] = useState({
@@ -78,164 +79,167 @@ const ComplaintsPage = () => {
     };
 
     return (
-        <div className="p-8 max-w-lg mx-auto mt-24">
-            <h1 className="text-3xl font-bold mb-4 text-primary text-left">Reklamasjonsskjema</h1>
+        <div className="p-8 max-w-6xl mx-auto mt-20 grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
+            <div className="">
+                {successMessage && <p className="text-green-600 text-center mb-4">{successMessage}</p>}
+                {errorMessage && <p className="text-red-600 text-center mb-4">{errorMessage}</p>}
+                <h1 className="text-3xl font-bold mb-6 text-primary text-left">Reklamasjonsskjema</h1>
 
-            {/* TODO: Legg til skjema */}
-            <p className="mb-6 text-gray-800 leading-relaxed text-left">
-                <strong>Slik sender du inn reklamasjon</strong><br />
-                Vi tar reklamasjoner på alvor og vil hjelpe deg så raskt som mulig. For å kunne behandle saken effektivt, trenger vi litt informasjon fra deg: bilens registreringsnummer, en beskrivelse av problemet og eventuelt dokumentasjon som bilder eller rapporter fra verksted. <br /><br />
-                Alt sendes inn via skjemaet nedenfor – og du får alltid svar skriftlig fra oss.
-            </p>
 
-            {successMessage && <p className="text-green-600 text-center mb-4">{successMessage}</p>}
-            {errorMessage && <p className="text-red-600 text-center mb-4">{errorMessage}</p>}
-
-            {/* Personlig info */}
-            <form onSubmit={handleSubmit} className="space-y-4">
-                {/* Steg 1. Navn */}
-                <div className="text-left">
-                    <div>
-                        <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Navn:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            required
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
-                        />
-                    </div>
-
-                    {/* Steg 2: E-post */}
-                    {/* {formData.name && ( */}
+                {/* Personlig info */}
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    {/* Steg 1. Navn */}
+                    <div className="text-left">
                         <div>
-                            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">E-post:</label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            />
-                        </div>
-                    {/* )} */}
-
-                    {/* Steg 3: Telefonnummer */}
-                    {/* {formData.email && ( */}
-                        <div>
-                            <label htmlFor="phoneNum" className="block text-gray-700 text-sm font-bold mb-2">Telefonnummer:</label>
-                            <input
-                                type="tel"
-                                id="phoneNum"
-                                name="phoneNum"
-                                value={formData.phoneNum}
-                                onChange={handleChange}
-                                required
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            />
-                        </div>
-                    {/* )} */}
-
-                    {/* Steg 4. Reg nummer */}
-                    {/* {formData.phoneNum && ( */}
-                        <div>
-                            <label htmlFor="regNum" className="block text-gray-700 text-sm font-bold mb-2">Registreringsnummer:</label>
+                            <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Navn:</label>
                             <input
                                 type="text"
-                                id="regNum"
-                                name="regNum"
-                                value={formData.regNum}
+                                id="name"
+                                name="name"
+                                value={formData.name}
                                 onChange={handleChange}
                                 required
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
                             />
                         </div>
-                    {/* )} */}
 
-                    {/* Steg 5. Beskrivelse */}
-                    {/* {formData.regNum && ( */}
+                        {/* Steg 2: E-post */}
+                        {/* {formData.name && ( */}
+                            <div>
+                                <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">E-post:</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    required
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </div>
+                        {/* )} */}
+
+                        {/* Steg 3: Telefonnummer */}
+                        {/* {formData.email && ( */}
+                            <div>
+                                <label htmlFor="phoneNum" className="block text-gray-700 text-sm font-bold mb-2">Telefonnummer:</label>
+                                <input
+                                    type="tel"
+                                    id="phoneNum"
+                                    name="phoneNum"
+                                    value={formData.phoneNum}
+                                    onChange={handleChange}
+                                    required
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </div>
+                        {/* )} */}
+
+                        {/* Steg 4. Reg nummer */}
+                        {/* {formData.phoneNum && ( */}
+                            <div>
+                                <label htmlFor="regNum" className="block text-gray-700 text-sm font-bold mb-2">Registreringsnummer:</label>
+                                <input
+                                    type="text"
+                                    id="regNum"
+                                    name="regNum"
+                                    value={formData.regNum}
+                                    onChange={handleChange}
+                                    required
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </div>
+                        {/* )} */}
+
+                        {/* Steg 5. Beskrivelse */}
+                        {/* {formData.regNum && ( */}
+                            <div>
+                                <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">Beskrivelse av problem/skade</label>
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    value={formData.description}
+                                    onChange={handleChange}
+                                    required
+                                    rows="5"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </div>
+                        {/* )} */}
+
+                        {/* Steg 6. Synlig skade */}
+                        {/* {formData.description && ( */}
+                            <div>
+                                <div className="flex items-center space-x-2 pt-4">
+                                    <input
+                                        type="checkbox"
+                                        id="visibleDamage"
+                                        name="visibleDamage"
+                                        onChange={handleChange}
+                                        className="form-checkbox h-4 w-4 text-primary"
+                                    />
+                                    <label htmlFor="visibleDamage" className="text-gray-700 text-sm">Er skaden synlig?</label>
+                                </div>
+                            </div>
+                        {/* )} */}
+                    </div>
+
+                    {/* Steg 7. Bildeopplasting */}
+                    {formData.visibleDamage && (
                         <div>
-                            <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">Beskrivelse av problem/skade</label>
-                            <textarea
-                                id="description"
-                                name="description"
-                                value={formData.description}
-                                onChange={handleChange}
+                            <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
+                                Bilde av skaden: <span className="text-red-500">*</span>
+                            </label>
+                            <input
+                                type="file"
+                                id="image"
+                                name="image"
+                                accept="image/*"
+                                onChange={handleFileChange}
                                 required
-                                rows="5"
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
                             />
                         </div>
-                    {/* )} */}
+                    )}
 
-                    {/* Steg 6. Synlig skade */}
+                    {/* Steg 8. Bildeopplasting */}
                     {/* {formData.description && ( */}
                         <div>
-                            <div className="flex items-center space-x-2 pt-4">
-                                <input
-                                    type="checkbox"
-                                    id="visibleDamage"
-                                    name="visibleDamage"
-                                    onChange={handleChange}
-                                    className="form-checkbox h-4 w-4 text-primary"
-                                />
-                                <label htmlFor="visibleDamage" className="text-gray-700 text-sm">Er skaden synlig?</label>
-                            </div>
+                            <label htmlFor="documentation" className="block text-gray-700 text-sm font-bold mb-2">
+                                Last opp dokumentasjon
+                            </label>
+                            <input
+                                type="file"
+                                id="documentation"
+                                name="documentation"
+                                multiple
+                                onChange={handleFileChange}
+                                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+                            />
                         </div>
                     {/* )} */}
-                </div>
 
-                {/* Steg 7. Bildeopplasting */}
-                {formData.visibleDamage && (
-                    <div>
-                        <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
-                            Bilde av skaden: <span className="text-red-500">*</span>
-                        </label>
-                        <input
-                            type="file"
-                            id="image"
-                            name="image"
-                            accept="image/*"
-                            onChange={handleFileChange}
-                            required
-                            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-                        />
+                    {/* Submit-knapp */}
+                    <div className="mt-6">
+                        <button type="submit" className="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
+                            Send inn →
+                        </button>
                     </div>
-                )}
-
-                {/* Steg 8. Bildeopplasting */}
-                {/* {formData.description && ( */}
-                    <div>
-                        <label htmlFor="documentation" className="block text-gray-700 text-sm font-bold mb-2">
-                            Last opp dokumentasjon
-                        </label>
-                        <input
-                            type="file"
-                            id="documentation"
-                            name="documentation"
-                            multiple
-                            onChange={handleFileChange}
-                            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-                        />
-                    </div>
-                {/* )} */}
-
-                {/* Submit-knapp */}
-                <div className="mt-6">
-                    <button type="submit" className="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
-                        Send inn →
-                    </button>
-                </div>
 
 
 
-                {/* Skadebeskrivelse og vedlegg */}
+                    {/* Skadebeskrivelse og vedlegg */}
 
-            </form>
+                </form>
+            </div>
+            <div className="mt-20">
+                <img src={complaintImage} alt="Reklamasjon. Bilde." className="rounded-lg mb-6 shadow-lg w-full object-cover" />
+                <p className="mb-6 text-gray-800 leading-relaxed text-left">
+                    <strong>Slik sender du inn reklamasjon</strong><br />
+                    Vi tar reklamasjoner på alvor og vil hjelpe deg så raskt som mulig. For å kunne behandle saken effektivt, trenger vi litt informasjon fra deg: bilens registreringsnummer, en beskrivelse av problemet og eventuelt dokumentasjon som bilder eller rapporter fra verksted. <br/> <br/>
+                    Alt sendes inn via reklmamasjonsskjemaet – og du får alltid svar skriftlig fra oss.
+                </p>
+            </div>
         </div>
     )
 }
