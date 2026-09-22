be4e7c656240727628b319092e9c4425ab50503c
elineviktoria <eline@tobiassen.no>
2026-09-22 18:22:25 +0200

    Update Login.jsx

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -134,7 +134,7 @@ const Login = () => {
                         to="/"
                         className="mx-auto inline-flex items-center justify-center rounded-full bg-primary-light px-5 py-2 text-sm font-semibold text-primary-dark transition-colors duration-300 hover:bg-primary hover:text-warm-off-white"
                     >
-                        ← Tilbake til nettsiden
+                        Tilbake til nettsiden
                     </Link>
                 </div>
             </div>
